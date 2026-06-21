062fa6c9fa9d018839a964f5803da3760b9a55a2
xu_su <xu_su@users.noreply.github.com>
2026-06-21 19:58:37 +0800

    feat: implement switch parsing (-a, -la, --all)

--- args-parser.ps1
+++ args-parser.ps1
@@ -14,5 +14,42 @@ function Parse-BashArgs {
         Errors = @()
     }
 
+    $i = 0
+    while ($i -lt $ArgsArray.Count) {
+        $arg = $ArgsArray[$i]
+
+        # 长参数: --option
+        if ($arg.StartsWith('--')) {
+            $longName = $arg.Substring(2)
+            foreach ($key in $OptionSpec.Keys) {
+                if ($OptionSpec[$key].Long -eq $longName) {
+                    $result.Options[$key] = $true
+                    $result.LongOptions[$longName] = $true
+                    break
+                }
+            }
+            $i++
+        }
+        # 短参数: -a 或 -la 组合
+        elseif ($arg.StartsWith('-') -and $arg.Length -gt 1) {
+            $flags = $arg.Substring(1)
+            for ($j = 0; $j -lt $flags.Length; $j++) {
+                $flag = [string]$flags[$j]
+                if ($OptionSpec.ContainsKey($flag)) {
+                    $result.Options[$flag] = $true
+                    if ($OptionSpec[$flag].Long) {
+                        $result.LongOptions[$OptionSpec[$flag].Long] = $true
+                    }
+                }
+            }
+            $i++
+        }
+        # 位置参数
+        else {
+            $result.Positional += $arg
+            $i++
+        }
+    }
+
     return $result
 }
--- tests/test-args-parser.ps1
+++ tests/test-args-parser.ps1
@@ -9,4 +9,28 @@ Describe "Parse-BashArgs" {
         $result.Options.Count | Should Be 0
         $result.Positional.Count | Should Be 0
     }
+
+    It "Parses single short switch -a" {
+        $spec = @{ 'a' = @{ Long = 'all'; Type = 'switch' } }
+        $result = Parse-BashArgs -ArgsArray @('-a') -OptionSpec $spec
+        $result.Options['a'] | Should Be $true
+        $result.LongOptions['all'] | Should Be $true
+    }
+
+    It "Parses combined short switches -la" {
+        $spec = @{
+            'l' = @{ Long = 'long'; Type = 'switch' }
+            'a' = @{ Long = 'all'; Type = 'switch' }
+        }
+        $result = Parse-BashArgs -ArgsArray @('-la') -OptionSpec $spec
+        $result.Options['l'] | Should Be $true
+        $result.Options['a'] | Should Be $true
+    }
+
+    It "Parses long option --all" {
+        $spec = @{ 'a' = @{ Long = 'all'; Type = 'switch' } }
+        $result = Parse-BashArgs -ArgsArray @('--all') -OptionSpec $spec
+        $result.Options['a'] | Should Be $true
+        $result.LongOptions['all'] | Should Be $true
+    }
 }
